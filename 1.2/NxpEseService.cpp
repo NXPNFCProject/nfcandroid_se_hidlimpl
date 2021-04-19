a7b1640e1312a8136b0465fcba6c74ec9cf6dc89
nxf58888 <himanshusingh.kushwah@nxp.com>
2021-04-19 11:50:30 +0530

    Add wait for NFC HAL Registeration
    - Wait fo NFC HIDL Registration before registering SE HAL
    - Retry 30 times with 100ms delay for NFC HAL Registration Check

--- 1.2/NxpEseService.cpp
+++ 1.2/NxpEseService.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright 2018-2020 NXP
+ *  Copyright 2018-2021 NXP
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@
 #include <log/log.h>
 #include <android/hardware/secure_element/1.2/ISecureElement.h>
 #include <vendor/nxp/nxpese/1.0/INxpEse.h>
+#include <android/hardware/nfc/1.2/INfc.h>
 #include "VirtualISO.h"
 
 #include <hidl/LegacySupport.h>
@@ -27,6 +28,9 @@
 #include "NxpEse.h"
 #include "SecureElement.h"
 #include "eSEClient.h"
+
+#define MAX_NFC_GET_RETRY           30
+#define NFC_GET_SERVICE_DELAY_MS    100
 
 // Generated HIDL files
 using android::hardware::secure_element::V1_2::ISecureElement;
@@ -40,10 +44,30 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::registerPassthroughServiceImplementation;
 using android::hardware::joinRpcThreadpool;
 using android::base::StringPrintf;
+using android::hardware::nfc::V1_2::INfc;
 
 using android::sp;
 using android::status_t;
 using android::OK;
+
+static inline void waitForNFCHAL() {
+  int retry = 0;
+  android::sp<INfc> nfc_service = nullptr;
+
+  ALOGI("Waiting for NFC HAL .. ");
+  do {
+     nfc_service = INfc::tryGetService();
+     if (nfc_service !=  nullptr) {
+       ALOGI("NFC HAL service is registered");
+       break;
+     }
+     /* Wait for 100 MS for HAL RETRY*/
+     usleep(NFC_GET_SERVICE_DELAY_MS * 1000);
+  } while (retry++ < MAX_NFC_GET_RETRY);
+  if(nfc_service == nullptr) {
+    ALOGE("Failed to get NFC HAL Service");
+  }
+}
 
 int main() {
   status_t status;
@@ -56,8 +80,9 @@ int main() {
   android::sp<INxpEse> nxp_se_service = nullptr;
   android::sp<ISecureElement> virtual_iso_service = nullptr;
 
-  ALOGI("Secure Element HAL Service 1.2 is starting.");
   try {
+    waitForNFCHAL();
+    ALOGI("Secure Element HAL Service 1.2 is starting.");
     se_service = new SecureElement();
     if (se_service == nullptr) {
       ALOGE("Can not create an instance of Secure Element HAL Iface, exiting.");
