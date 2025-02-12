939d03d482abed59b4316bb1f73566c9bb6d4ed3
nxf45198 <fahim.akhtar@nxp.com>
2025-02-12 16:14:12 +0530

    Update the copyright
    - Updated the copyright

--- snxxx/libese-teq1/src/adaptation/CondVar.cpp
+++ snxxx/libese-teq1/src/adaptation/CondVar.cpp
@@ -30,7 +30,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- *  Copyright 2024 NXP
+ *  Copyright 2024-2025 NXP
  *
  ******************************************************************************/
 
