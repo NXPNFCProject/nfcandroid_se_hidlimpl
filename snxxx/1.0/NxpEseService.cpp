6338ce88f30092cfa3a72a757d7f7e38c3e1b96d
nxf97491 <ramyesh.thanthri@nxp.com>
2025-06-13 19:35:39 +0530

    Replace base/logging.h with android-base/logging.h
    As part of the effort to uprev libchrome in Android, we are
    removing Android dependencies to libchrome utility functions
    that are not strictly required.
    
    This is an incremental step toward removing base/logging.h entirely.
    
    Bug: 360917504
    Test: build

--- snxxx/1.0/NxpEseService.cpp
+++ snxxx/1.0/NxpEseService.cpp
@@ -16,9 +16,9 @@
  *
  ******************************************************************************/
 #define LOG_TAG "nxpese@1.0-service"
+#include <android-base/logging.h>
 #include <android-base/stringprintf.h>
 #include <android/hardware/secure_element/1.0/ISecureElement.h>
-#include <base/logging.h>
 #include <hidl/LegacySupport.h>
 #include <string.h>
 #include <vendor/nxp/nxpese/1.0/INxpEse.h>
