df9e9ddd5e8c70c98e8d0c44285a093d079df7c9
nxf38293 <lokanathareddy.virayagarri@nxp.com>
2025-09-10 15:55:50 +0530

    Fix for protocol error when LRC error is received.
    When parity error is received for previous frame, shall resend
    the same frame again.

--- snxxx/libese-teq1/nxp-ese/lib/phNxpEseProto7816_3.cpp
+++ snxxx/libese-teq1/nxp-ese/lib/phNxpEseProto7816_3.cpp
@@ -1249,7 +1249,9 @@ static ESESTATUS phNxpEseProto7816_DecodeFrame(uint8_t* p_data,
         break;
       case RESYNCH_RSP:
         if (phNxpEseProto7816_3_Var.phNxpEseRx_Cntx.lastRcvdRframeInfo
-                .errCode == OTHER_ERROR) {
+                .errCode == OTHER_ERROR ||
+            phNxpEseProto7816_3_Var.phNxpEseRx_Cntx.lastRcvdRframeInfo
+                .errCode == PARITY_ERROR) {
           phNxpEseProto7816_3_Var.phNxpEseRx_Cntx.lastRcvdSframeInfo
               .sFrameType = RESYNCH_RSP;
           phNxpEseProto7816_3_Var.phNxpEseRx_Cntx.lastRcvdRframeInfo.errCode =
