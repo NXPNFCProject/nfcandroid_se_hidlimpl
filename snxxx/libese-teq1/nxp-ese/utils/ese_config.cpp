96a4d62bad45a8a354f4610452c8247a333677a2
Sai Shweta S <sai.shwethas@nxp.com>
2025-12-10 11:38:36 +0100

    Revert "[artf1301890]: Reuse utils for libese_vendor_extn"
    This reverts commit 10b5a7c3a363e51564f5d12165df5055db0ae12d.
    
    Reason for revert: <We cannot remove this as the SE HAL utils uses libese-nxp.conf file config reading which will not be present in SN220V5E chipset>
    
    Change-Id: I45e51e6bb05ab20c751724c95819470f671f0fdf

--- snxxx/libese-teq1/nxp-ese/utils/ese_config.cpp
+++ snxxx/libese-teq1/nxp-ese/utils/ese_config.cpp
@@ -79,14 +79,3 @@ unsigned EseConfig::getUnsigned(const std::string& key,
   if (hasKey(key)) return getUnsigned(key);
   return default_value;
 }
-
-unsigned long EseConfig::getUnsignedLong(const std::string& key) {
-  std::string s = getString(key);
-  return std::stoul(s);
-}
-
-unsigned long EseConfig::getUnsignedLong(const std::string& key,
-                                unsigned long default_value) {
-  if (hasKey(key)) return getUnsignedLong(key);
-  return default_value;
-}
