b7d65f4fd985eccb7bd6dbcfaf57b42306466cf4
nxf31698 <suryaprakash.konduru@nxp.com>
2026-06-11 21:07:15 +0530

    Updated MW version with 17.07.00_TC1
    Change-Id: I4417716e303f42f3dc8a077b91b8a5edd8aad1de

--- snxxx/libese-teq1/nxp-ese/lib/phNxpEse_Api.cpp
+++ snxxx/libese-teq1/nxp-ese/lib/phNxpEse_Api.cpp
@@ -47,7 +47,7 @@
 #define NXP_EN_SN300U 1
 #define NXP_EN_SN330U 1
 #define NFC_NXP_MW_ANDROID_VER (17U)  /* Android version used by NFC MW */
-#define NFC_NXP_MW_VERSION_MAJ (0x06) /* MW Major Version */
+#define NFC_NXP_MW_VERSION_MAJ (0x07) /* MW Major Version */
 #define NFC_NXP_MW_VERSION_MIN (0x00) /* MW Minor Version */
 #define NFC_NXP_MW_CUSTOMER_ID (0x00) /* MW Customer Id */
 #define NFC_NXP_MW_RC_VERSION (0x00)  /* MW RC Version */
@@ -87,7 +87,7 @@ static void printSeHalVersion() {
   validation |= (NXP_EN_SN330U << 18);
   validation |= (NXP_EN_PN557 << 11);
 
-  NXP_LOG_ESE_D("SE HAL Version: NFC_AR_%02X_%05X_%02d.%02x.%02x",
+  NXP_LOG_ESE_D("SE HAL Version: NFC_AR_%02X_%05X_%02d.%02x.%02x_TC1",
                 NFC_NXP_MW_CUSTOMER_ID, validation, NFC_NXP_MW_ANDROID_VER,
                 NFC_NXP_MW_VERSION_MAJ, NFC_NXP_MW_VERSION_MIN);
 }
